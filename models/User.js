0d07db4162a2b833f44d0decfd7236d63de06ee0
Baysangur Delbiev <111337500+Boisglar@users.noreply.github.com>
2023-04-15 15:34:59 +0300

    инициализация бэка, создание поль и автр

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import { mongoose } from 'mongoose';
+
+const userShema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      require: true,
+    },
+    avatarUrl: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('User', userShema);
